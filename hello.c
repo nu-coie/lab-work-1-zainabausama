a0b4f424ff0300014600d8a5af1dfbcaee8d0388
zainabausama <130349456+zainabausama@users.noreply.github.com>
2023-04-16 19:08:07 +0000

    Sun, Apr 16, 2023, 7:08 PM +00:00

--- hello.c
+++ hello.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
-
+#include <cs50>
 int main(void)
 {
-  
-  // Step ( 1 ) : Print Hello, World
-  
-  
-  
-  //Step ( 2 ) : Print Hello, Your Name
-
+  printf("hello,world\n");
 }
